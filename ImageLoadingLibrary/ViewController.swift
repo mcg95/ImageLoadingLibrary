1aa1e46af1709ea1592e2b7fb1fe3da8fdda5414
mewanchathuranga <mc_gamer@live.com>
2018-06-12 11:53:31 +0800

    Initial Commit

---
+++ ImageLoadingLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLoadingLibrary
+//
+//  Created by Mewan Chathuranga on 12/06/2018.
+//  Copyright © 2018 Mewan Chathuranga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImageLoadingLibraryTests/ImageLoadingLibraryTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageLoadingLibraryTests.swift
+//  ImageLoadingLibraryTests
+//
+//  Created by Mewan Chathuranga on 12/06/2018.
+//  Copyright © 2018 Mewan Chathuranga. All rights reserved.
+//
+
+import XCTest
+@testable import ImageLoadingLibrary
+
+class ImageLoadingLibraryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
