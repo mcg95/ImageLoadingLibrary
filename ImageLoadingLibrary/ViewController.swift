ea92ace6ee5cbdba95b9194dad47ca0e81ba2990
mewanchathuranga <mc_gamer@live.com>
2018-06-13 03:15:40 +0800

    Decoding and Extracting image URLs from JSON.

--- ImageLoadingLibrary/ViewController.swift
+++ ImageLoadingLibrary/ViewController.swift
@@ -8,11 +8,67 @@
 
 import UIKit
 
+struct ImageKit: Decodable{
+//    let profileImages: [profileImage]
+    let urls: userImageURLS
+
+  
+}
+
+struct userImageURLS:Decodable{
+    
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
+    
+  /*  private enum CodingKeys: String, CodingKey{
+        
+        case rawURL = "raw"
+        case fullURL = "full"
+        case regularURL = "regular"
+        case smallURL = "small"
+        case thumbURL = "thumb"
+    }*/
+}
+struct profileImage: Decodable{
+    let profileImageSmlURL: String
+    let profileImageMedURL: String
+    let profileImageLrgURL: String
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonURL = "https://pastebin.com/raw/wgkJgazE"
+        
+        guard let url = URL(string: jsonURL) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else {
+                return
+            }
+            
+            do{
+                let json = try JSONDecoder().decode([ImageKit].self, from: data)
+                for imageURL in json{
+                    print(imageURL.urls.small)
+                }
+            }
+            catch{
+                print(error)
+            }
+        
+            
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
